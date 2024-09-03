f4ad4a128d3a4f3406e23cbbad5bf2e3dacf2b3e
Cedric GESTES <cedric.gestes@flex.ai>
2024-09-03 15:18:50 +0000

    hipify: add cudaLaunchHostFunc

--- torch/utils/hipify/cuda_to_hip_mappings.py
+++ torch/utils/hipify/cuda_to_hip_mappings.py
@@ -5023,6 +5023,7 @@ CUDA_IDENTIFIER_MAP = collections.OrderedDict(
             "cudaLaunchCooperativeKernel",
             ("hipLaunchCooperativeKernel", CONV_EXEC, API_RUNTIME),
         ),
+        ("cudaLaunchHostFunc", ("hipLaunchHostFunc", CONV_EXEC, API_RUNTIME, HIP_UNSUPPORTED)),
         (
             "cudaSetupArgument",
             ("hipSetupArgument", CONV_EXEC, API_RUNTIME, HIP_UNSUPPORTED),
